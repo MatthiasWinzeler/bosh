eebc9c8a028f450a631da5aecda384450817db2e
Kai Hofstetter <kai.hofstetter@sap.com>
2018-08-14 16:40:11 +0200

    Improve AuditLogger integration test performance

--- src/spec/gocli/integration/auditlog_spec.rb
+++ src/spec/gocli/integration/auditlog_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
 
 describe 'Audit log', type: :integration do
-  with_reset_sandbox_before_each
+  with_reset_sandbox_before_all
 
-  let(:audit_director_log) { File.join(current_sandbox.logs_path, 'audit.log') }
-
-  let(:audit_worker_0_log) { File.join(current_sandbox.logs_path, 'audit_worker_0.log') }
-  let(:audit_worker_1_log) { File.join(current_sandbox.logs_path, 'audit_worker_1.log') }
-  let(:audit_worker_2_log) { File.join(current_sandbox.logs_path, 'audit_worker_2.log') }
-
-  before do
+  before(:all) do
     deploy_from_scratch(
       manifest_hash: Bosh::Spec::NewDeployments.simple_manifest_with_instance_groups,
       cloud_config_hash: Bosh::Spec::NewDeployments.simple_cloud_config,
     )
   end
+
+  let(:audit_director_log) { File.join(current_sandbox.logs_path, 'audit.log') }
+  let(:audit_worker_0_log) { File.join(current_sandbox.logs_path, 'audit_worker_0.log') }
+  let(:audit_worker_1_log) { File.join(current_sandbox.logs_path, 'audit_worker_1.log') }
+  let(:audit_worker_2_log) { File.join(current_sandbox.logs_path, 'audit_worker_2.log') }
 
   it 'writes audit logs' do
     expect(File).to exist(audit_director_log)
