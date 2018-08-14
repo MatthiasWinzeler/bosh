1b53d6ad6c7a0d4479753c4ff0fdf78c31bbaa56
Kai Hofstetter <kai.hofstetter@sap.com>
2018-08-14 15:57:29 +0200

    Add AuditLogger integration tests

--- src/spec/gocli/integration/auditlog_spec.rb
+++ src/spec/gocli/integration/auditlog_spec.rb
@@ -1,24 +1,39 @@
 require 'spec_helper'
 
-describe 'Audit logs', type: :integration do
+describe 'Audit log', type: :integration do
   with_reset_sandbox_before_each
 
-  it 'writes audit logs', focus: true do
+  let(:audit_director_log) { File.join(current_sandbox.logs_path, 'audit.log') }
+
+  let(:audit_worker_0_log) { File.join(current_sandbox.logs_path, 'audit_worker_0.log') }
+  let(:audit_worker_1_log) { File.join(current_sandbox.logs_path, 'audit_worker_1.log') }
+  let(:audit_worker_2_log) { File.join(current_sandbox.logs_path, 'audit_worker_2.log') }
+
+  before do
     deploy_from_scratch(
       manifest_hash: Bosh::Spec::NewDeployments.simple_manifest_with_instance_groups,
       cloud_config_hash: Bosh::Spec::NewDeployments.simple_cloud_config,
     )
-
-    # expect('sandbox_path'). to eq(current_sandbox.sandbox_path('logs'))
-    expect(File).to exist(File.join(current_sandbox.logs_path, 'audit.log'))
-    expect(File).to exist(File.join(current_sandbox.logs_path, 'audit_worker_0.log'))
-    expect(File).to exist(File.join(current_sandbox.logs_path, 'audit_worker_1.log'))
-    expect(File).to exist(File.join(current_sandbox.logs_path, 'audit_worker_2.log'))
   end
 
-  it 'contain request logs' do
-    audit_log = File.open(File.join(current_sandbox.logs_path, 'audit.log')).read
-    # expect(audit_log).to match(/.*CEF.*\/info.*/)
-    expect(audit_log).to match(/.*DirectorAudit.*deadbeef.*/)
+  it 'writes audit logs' do
+    expect(File).to exist(audit_director_log)
+    expect(File).to exist(audit_worker_0_log)
+    expect(File).to exist(audit_worker_1_log)
+    expect(File).to exist(audit_worker_2_log)
+  end
+
+  it 'contains request logs' do
+    audit_log_content = File.open(audit_director_log).read
+
+    expect(audit_log_content).to match(/^.*CEF.*|\\deployments|.*requestMethod=POST.*$/)
+  end
+
+  it 'contains event logs' do
+    worker_logs_content = File.open(audit_worker_0_log).read +
+                          File.open(audit_worker_1_log).read +
+                          File.open(audit_worker_2_log).read
+
+    expect(worker_logs_content).to match(/^.*"action":"create".*"object_type":"deployment".*$/)
   end
 end
