08d95c72fc975dc93aafd509bd8b44f40c296870
Kai Hofstetter <kai.hofstetter@sap.com>
2018-08-15 08:37:02 +0200

    Refactor AuditLogger

--- src/bosh-director/lib/bosh/director/audit_logger.rb
+++ src/bosh-director/lib/bosh/director/audit_logger.rb
@@ -4,14 +4,12 @@ module Bosh::Director
   class AuditLogger
 
     def initialize
-      log_file_path = Config.audit_log_path
-
       @logger = Logging::Logger.new('DirectorAudit')
       @logger.level = 'debug'
       @logger.add_appenders(
         Logging.appenders.file(
           'DirectorAudit',
-          filename: File.join(log_file_path, Config.audit_filename),
+          filename: File.join(Config.audit_log_path, Config.audit_filename),
           layout: ThreadFormatter.layout,
         ),
       )
